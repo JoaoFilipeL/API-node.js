a801e7fb64cbc88e7b0a21c5cd32c65c11afdb38
João Filipe <joaofcliotto@gmail.com>
2024-08-23 02:45:25 -0300

    NODE API

---
+++ config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'usuario',
+    password: '123',
+    database: 'api-node',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}
