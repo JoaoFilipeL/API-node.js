06cfd1cfd85e81b010d25f99c26dcec6810315ca
João Filipe <joaofcliotto@gmail.com>
2024-08-23 21:40:54 -0300

    apiNode

---
+++ controllers/EnderecoController.js
@@ -0,0 +1,119 @@
+const axios = require('axios');
+const {Endereco} = require('../models');
+
+exports.createEnderecoCep = async (req, res) => {
+    try {
+        const { cep } = req.params;
+        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+
+        if (response.data.erro) {
+            return res.status(404).json({ error: 'CEP não encontrado' });
+        }
+
+        const { logradouro, complemento, bairro, localidade, uf, ibge } = response.data;
+
+        novoEndereco = await Endereco.create({
+            Cep: cep,
+            Logradouro: logradouro,
+            Numero: req.body.Numero,
+            Complemento: complemento,
+            Bairro: bairro,
+            Cidade: localidade,
+            Estado: uf,
+            MunicipioIBGE: ibge,
+        });
+
+        res.status(201).json(novoEndereco);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar o endereço', details: error.message});
+    }};
+
+exports.createEndereco = async (req, res) => {
+    try {
+        const {Cep, Logradouro, Numero, Complemento, Bairro, Cidade, Estado, MunicipioIBGE} = req.body;
+
+        const novoEndereco = await Endereco.create({
+            Cep,
+            Logradouro,
+            Numero,
+            Complemento,
+            Bairro,
+            Cidade,
+            Estado,
+            MunicipioIBGE,
+        });
+
+        res.status(201).json(novoEndereco);
+    }catch(error) {
+        res.status(500).json({ error: 'Erro ao criar endereço', details: error.message});
+    }
+};
+
+exports.getAllEnderecos = async (req, res) => {
+    try {
+        const enderecos = await Endereco.findAll();
+        res.status(200).json(enderecos);
+    }catch(error) {
+        res.status(500).json({ error: 'Erro ao buscar endereços', details: error.message});
+    }
+};
+
+exports.getEnderecoById = async (req, res) => {
+    try {
+        const { Id } = req.params;
+        const endereco = await Endereco.findByPk(Id);
+
+        if (!endereco) {
+            return res.status(404).json({ error: 'Endereço não encontrado'});
+        }
+
+        res.status(200).json(enderecos);
+    }catch(error) {
+        res.status(500).json({ error: 'Erro ao buscar endereço', details: error.message});
+    }
+}
+
+exports.updateEndereco = async (req, res) => {
+    try {
+        const { Id } = req.params;
+        const {Cep, Logradouro, Numero, Complemento, Bairro, Cidade, Estado, MunicipioIBGE} = req.body;
+
+        const endereco = await Endereco.findByPk(Id);
+
+        if (!endereco) {
+            return res.status(404).json({ error: 'Endereço não encontrado'})
+        }
+
+        endereco.Cep = Cep;
+        endereco.Logradouro = Logradouro;
+        endereco.Numero = Numero;
+        endereco.Complemento = Complemento;
+        endereco.Bairro = Bairro;
+        endereco.Cidade = Cidade;
+        endereco.Estado = Estado;
+        endereco.MunicipioIBGE = MunicipioIBGE;
+
+        await endereco.save();
+
+        res.status(200).json(enderecos)
+    }catch(error) {
+        res.status(500).json({ error: 'Erro ao buscar endereço', details: error.message})
+    }
+}
+
+exports.deleteEndereco = async (req, res) => {
+    try {
+        const { Id } = req.params;
+        const endereco = await Endereco.findByPk(Id)
+
+        if (!endereco) {
+            return res.status(404).json({ error: 'Endereço não encontrado'})
+        }
+
+        await endereco.destroy();
+
+        res.status(204).send();
+    }catch(error) {
+        res.status(500).json({ error: 'Erro ao buscar endereço', details: error.message})
+    }
+}
---
+++ routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const enderecoController = require('./controllers/EnderecoController');
+const router = express.Router();
+
+router.post('/enderecos/cep/:cep', enderecoController.createEnderecoCep);
+router.post('/enderecos', enderecoController.createEndereco);
+router.get('/enderecos', enderecoController.getAllEnderecos);
+router.get('/enderecos', enderecoController.getEnderecoById);
+router.put('/enderecos', enderecoController.updateEndereco);
+router.delete('/enderecos', enderecoController.deleteEndereco);
+
+module.exports = router;
